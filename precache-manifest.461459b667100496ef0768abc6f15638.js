b10357356a1cafdb5442d401be196d852114a371
alinutzaB <alinutza.ms@gmail.com>
2019-11-04 10:23:39 +0200

    Updates

---
+++ precache-manifest.461459b667100496ef0768abc6f15638.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f78b07202c8726799f6298ad16319eb",
+    "url": "/react-tutorial/index.html"
+  },
+  {
+    "revision": "dfa2be466ae2d38ccfcf",
+    "url": "/react-tutorial/static/css/main.593a002d.chunk.css"
+  },
+  {
+    "revision": "c87132e000346e0aa072",
+    "url": "/react-tutorial/static/js/2.20c627ad.chunk.js"
+  },
+  {
+    "revision": "dfa2be466ae2d38ccfcf",
+    "url": "/react-tutorial/static/js/main.29da7b8a.chunk.js"
+  },
+  {
+    "revision": "f883517156692dea2325",
+    "url": "/react-tutorial/static/js/runtime-main.d8d52915.js"
+  }
+]);
